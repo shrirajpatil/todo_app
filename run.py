8f3513d4f4ab2efa1062f1113733984db6bb8a34
Shriraj Patil <91754780+shrirajpatil@users.noreply.github.com>
2024-12-15 18:31:47 +0530

    run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import app
-from models import db
+from models import get_db_connection  # Import your DB connection function
 
 if __name__ == '__main__':
     # Create database tables if they don't exist
     with app.app_context():
-        db.create_all()
+        get_db_connection.create_all()
     # Run the Flask application
     app.run(debug=True)
